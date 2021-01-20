8359aece0a2a11ba211ac00297e9950c1f884114
vitaliy kucher <koocherov@ukr.net>
2021-01-20 17:01:54 +0200

    Update to django 2.2.8

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url, include
 from core.views import *
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
 
     url(r'^fb_login/$', fb_login),
     url(r'^login/$', login_page, name='login_page'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^ticket/$', bug_ticket, name='bug_ticket'),
     url(r'^profile/my-petitions/$', my_petitions, name='my-petitions'),
     url(r'^profile/my-signed-petitions/$', my_signed_petitions, name='my-signed-petitions'),
